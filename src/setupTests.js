38e749026f4fa71bf8798f7a35f71db7a3ff0b0f
김태완 <aptakqmf12@gmail.com>
2023-12-01 16:41:21 +0900

    msw 에러 우회처리

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,7 +5,7 @@
 import "@testing-library/jest-dom";
 import { server } from "./mocks/server";
 
-beforeAll(() => server.listen());
+beforeAll(() => server.listen({ onUnhandledRequest: "bypass" })); // 처리되지않는 요청이 있으면 우회처리
 
 afterEach(() => server.resetHandlers());
 
